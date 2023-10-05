7795fd36f667fd62076d0cbe172b30dbf236ecc2
atslhamny <atsilahamanyph@gmail.com>
2023-10-05 08:34:39 +0700

    modifikasi 2

--- Pemilihan2Percobaan104.java
+++ Pemilihan2Percobaan104.java
@@ -10,8 +10,8 @@ public static void main(String[] args) {
     System.out.println("Masukkan Tahun : ");
     tahun = input04.nextInt();
 
-    if ((tahun % 4) == 0) {
-        if ((tahun % 100) != 0) {
+    if ((tahun % 400) == 0) {
+        if ((tahun % 100) == 0) {
             System.out.println("Tahun Kabisat");
         }
     } else {
